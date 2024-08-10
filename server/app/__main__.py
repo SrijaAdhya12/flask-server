c2db52ca4ed2fbe31a814208a8d36a7f211923c2
Srija Adhya <srijaadhya.11sc.2020@gmail.com>
2024-08-10 11:50:51 +0530

    fix: main.py

--- server/app/__main__.py
+++ server/app/__main__.py
@@ -1,8 +1,12 @@
 from app import create_app
+import os
 
 app = create_app()
 
 def main():
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
     app.run()
 
 if __name__ == "__main__":
